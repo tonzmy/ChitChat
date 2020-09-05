b25e6213f43d1785ec95c58a2d32074ba26f479b
tonzmy <ultracell@outlook.com>
2020-09-05 12:25:21 -0400

    beta

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,21 @@
 import React, {useState, useEffect} from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
+import InfoBar from '../InfoBar/InfoBar'
+import Input from '../Input/Input'
+import Messages from '../Messages/Messages.js'
+import './Chat.css'
 
 let socket
+
 
 const Chat = ({location}) => {
     const [name, setName] = useState('')
     const [room, setRoom] = useState('')
+    // single message
+    const [message, setMessage] = useState('')
+    // store all messages
+    const [messages, setMessages] = useState([])
     const ENDPOINT = 'localhost:5000'
 
     useEffect(() => {
@@ -20,11 +29,15 @@ const Chat = ({location}) => {
         socket = io(ENDPOINT)
 
         setName(name)
-        setName(room)
+        setRoom(room)
 
         // console.log(socket)
-        socket.emit('join', {name, room}, ({error}) => {
-            alert(error)
+        // join the room
+        socket.emit('join', {name, room}, (error) => {
+            if (error) {
+                alert(error)
+            }
+            
         })
          
         // unmountiing
@@ -34,9 +47,34 @@ const Chat = ({location}) => {
         }
     }, [ENDPOINT, location.search])
 
+    useEffect(() => {
+        
+        socket.on('message', (message) => {
+            // console.log("call useEffect", messages)
+            setMessages([...messages, message]);
+        })
+    }, [messages])
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if (message) {
+            socket.emit('sendMessage', message, ()=> setMessage(''));
+        }
+    }
+
+    console.log("message", message)
+    console.log("messages", messages)
+    
 
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <InfoBar room={room}/>
+                <Messages messages={messages} name={name}/>
+                <Input message={message}  setMessage ={setMessage} sendMessage={sendMessage}/>
+            </div>
+        </div>
     )
 }
 
